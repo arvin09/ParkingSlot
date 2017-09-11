b2d0228f41cf67f7082b5b6f219932d0e2931567
Awaghma <Arvind.Waghmare@morningstar.com>
2017-09-12 00:30:00 +0530

    Initial commit

---
+++ src/app/app-home.component.js
@@ -0,0 +1,5 @@
+import appModule from './app.module';
+
+appModule.component('appHome', {
+  template: require('./app-home.component.html')
+});
---
+++ src/app/app.module.js
@@ -0,0 +1,17 @@
+
+// jquery is required for bootstrap
+import $ from 'jquery'
+
+// bootstrap
+import 'bootstrap'
+
+// bootstrap css 
+import 'bootstrap/dist/css/bootstrap.css'
+
+import angular from 'angular';
+
+import '../style/app.css';
+
+let app =  angular.module('app', []);
+
+export default app;
