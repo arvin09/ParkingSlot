761dff985aae1297d02aafe45b4505cf11d6150f
Awaghma <Arvind.Waghmare@morningstar.com>
2017-09-12 09:21:11 +0530

    configured basic ui-router

--- src/app/app-home.component.js
+++ src/app/app-home.component.js
@@ -3,3 +3,13 @@ import appModule from './app.module';
 appModule.component('appHome', {
   template: require('./app-home.component.html')
 });
+
+
+appModule.component('appAbout', {
+  template: '<div>About</div>'
+});
+
+
+appModule.component('appLogin', {
+  template: '<div>Login</div>'
+});
--- src/app/app.module.js
+++ src/app/app.module.js
@@ -10,10 +10,25 @@ import 'bootstrap/dist/css/bootstrap.css'
 
 import angular from 'angular';
 
-import uirouter from '@uirouter/core';
+import uiRouter from '@uirouter/angularjs';
 
 import '../style/app.css';
 
-let app =  angular.module('app', [uirouter]);
+let app =  angular.module('app', [uiRouter]);
+
+app.config(function($stateProvider, $urlServiceProvider) {
+    $urlServiceProvider.rules.otherwise({ state: 'about' });
+    
+    $stateProvider.state('about', {
+      url: '/about',
+      component: 'appAbout'
+    });
+      
+    $stateProvider.state('login', {
+      url: '/login',
+      component: 'appLogin'
+    });
+  
+  });
 
 export default app;
